0a8485772e1094a9fa28cb97cd526feb1db5b1b2
Nkosenhle Duma <personal.godsonduma@gmail.com>
2020-08-11 19:19:31 +0200

    Printing availble space

--- etc/entry.sh
+++ etc/entry.sh
@@ -8,6 +8,10 @@ bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
 # Change hostname on first launch (you can comment this out if it has done it's purpose)
 #sed -i -e 's/{{SERVER_HOSTNAME}}/'"${SRCDS_HOSTNAME}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
 
+# show available space
+bash "ls -la"
+bash "df -h"
+
 ${STEAMAPPDIR}/srcds_run -game csgo -console -autoupdate -steam_dir ${STEAMCMDDIR} -steamcmd_script ${STEAMAPPDIR}/csgo_update.txt -usercon +fps_max $SRCDS_FPSMAX \
 			-tickrate $SRCDS_TICKRATE -port $SRCDS_PORT +tv_port $SRCDS_TV_PORT +clientport $SRCDS_CLIENT_PORT -maxplayers_override $SRCDS_MAXPLAYERS +game_type $SRCDS_GAMETYPE +game_mode $SRCDS_GAMEMODE \
 			+mapgroup $SRCDS_MAPGROUP +map $SRCDS_STARTMAP +sv_setsteamaccount $SRCDS_TOKEN +rcon_password $SRCDS_RCONPW +sv_password $SRCDS_PW +sv_region $SRCDS_REGION +sv_lan $SRCDS_LAN
